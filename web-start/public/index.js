c67f38655c000dcf5b7fd5b29cbcb229f4449427
ronya <ronyab@kth.se>
2019-05-20 21:35:10 +0200

    markers borde funka men dom vill inte :,,,,,(

--- web-start/public/index.js
+++ web-start/public/index.js
@@ -1,16 +1,116 @@
-var map;
+var map, infoWindow;
 var icon = "marker.png";
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 59.3498092, lng: 18.0684758 },
-    zoom: 17,
+    zoom: 15,
     mapTypeId: "roadmap",
     tilt: 0
-    //tilt: 45
   });
+  infoWindow = new google.maps.InfoWindow;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
-  var marker1 = new google.maps.Marker({
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Enemy spotted.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+/*var map, infoWindow;
+function initMap1() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 59.3498065, lng: 18.0706646},
+      zoom: 15,
+      mapTypeId: "roadmap",
+      titlt: 0
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Enemy spotted.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+}*/
+
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  }
+
+  function addMarker(location, titel) {
+    console.log("location " + location);
+    marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        icon: icon,
+        title: titel,
+    });
+    marker.addListener("click", function() {
+      map.setZoom(16);
+      map.setCenter(this.getPosition(location));
+      markerClick.open(map, this);
+    });
+}
+
+Stadsbiblioteket = new google.maps.LatLng(59.3434, 18.0548, 'aaa');
+addMarker(Stadsbiblioteket);
+ChaTalk = new google.maps.LatLng(59.34037, 18.057978, '2');
+addMarker(ChaTalk);
+Nymble = new google.maps.LatLng(59.34721, 18.070866, '3');
+addMarker(Nymble);
+Kaferang = new google.maps.LatLng(59.32343754999999, 18.06, '4');
+addMarker(Kaferang);
+
+var InfoContent =
+'<div id="content">' +
+'<div id="siteNotice">' +
+"</div>" +
+'<div id="bodyContent">' +
+"<p><b>Koppla till onsen card somehow, visa info</b></p>" +
+"</div>";
+
+
+var markerClick = new google.maps.InfoWindow({
+content: InfoContent
+});
+
+/*Nymble.addListener("click", function() {
+  map.setZoom(8);
+  map.setCenter(marker.getPosition());
+  stadsClick.open(map, stadsClick);
+});*/
+
+  /*var marker1 = new google.maps.Marker({
     position: { lat: 59.3434, lng: 18.0548 },
     map: map,
     title: "Stadsbiblioteket",
@@ -38,45 +138,9 @@ function initMap() {
   });
   marker3.addListener("click", function() {
     infowindow3.open(map, marker3);
-  });
+  });*/
 
-  var marker1String =
-    '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<div id="bodyContent">' +
-    "<p><b>Stadsbiblioteket</b></p>" +
-    "</div>";
 
-  var marker2String =
-    '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<div id="bodyContent">' +
-    "<p><b>Cha Talk</b></p>" +
-    "</div>";
-
-  var marker3String =
-    '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<div id="bodyContent">' +
-    "<p><b>Nymble</b></p>" +
-    "</div>";
-
-  var infowindow = new google.maps.InfoWindow({
-    content: marker1String
-  });
-  var infowindow2 = new google.maps.InfoWindow({
-    content: marker2String
-  });
-  var infowindow3 = new google.maps.InfoWindow({
-    content: marker3String
-  });
-
-  marker1.setMap(map);
-  marker2.setMap(map);
-  marker3.setMap(map);
 
   function CenterControl(controlDiv, map) {
     // Set CSS for the control border.
@@ -146,3 +210,4 @@ function showValue(slider) {
   output = document.getElementById(slider.id + "Output");
   output.innerHTML = slider.value; // Display the default slider value
 }
+
